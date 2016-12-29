f45291c01366fb687494067cb7712a434db99740
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-12-28 21:58:36 -0800

    Finish naive solution

--- sumLists/sumLists.py
+++ sumLists/sumLists.py
@@ -5,18 +5,6 @@ class Node(object):
     self.value = value
     self.next = None
 
-  # @property
-  # def value(self):
-  #   return self._value
-
-  # @value.setter
-  # def value(self, value):
-  #   self._value = value
-
-  # @value.deleter
-  # def value(self):
-  #   del self._value
-
 class LinkedList(object):
   def __init__(self):
     self.head = None
@@ -24,12 +12,12 @@ class LinkedList(object):
 
   def __str__(self):
     i = self.head
-    res = ''
+    res = '('
     while(i != self.tail):
-      res += i.value + ' -> '
+      res += str(i.value) + ' -> '
       i = i.next
 
-    res += i.value
+    res += str(i.value) + ')'
     return res
 
   def append(self, value):
@@ -41,15 +29,51 @@ class LinkedList(object):
       self.tail.next = n
       self.tail = n
 
+# #Simple test for Node
+# node = Node('Oh')
+# print node.value
+# print 'Testing List'
 
-node = Node('Oh')
-print node.value
-print 'Testing List'
+# #Simple test for LinkedList
+# a = LinkedList()
+# a.append('Oh')
+# a.append('Wat')
+# a.append('Dup')
+# print 'Head value: {}'.format(a.head.value)
+# print 'Tail value: {}'.format(a.tail.value)
+# print a
 
-a = LinkedList()
-a.append('Oh')
-a.append('Wat')
-a.append('Dup')
-print 'Head value: {}'.format(a.head.value)
-print 'Tail value: {}'.format(a.tail.value)
-print a
+A = LinkedList()
+A.append(5)
+A.append(1)
+A.append(6)
+
+B = LinkedList()
+B.append(3)
+B.append(4)
+B.append(1)
+
+def sumLists(a, b):
+  a = a.head
+  b = b.head
+  res = 0
+  #Gets the value from the first list
+  ten = 1
+  while(a.next != None):
+    res += a.value * ten
+    ten *= 10
+    a = a.next
+  res += a.value * ten
+
+  #Gets the value from the second list
+  ten = 1
+  while(b.next != None):
+    res += b.value * ten
+    ten *= 10
+    b = b.next
+  res += b.value * ten
+
+  return res
+
+print 'Testing {} + {}: {}'.format(A, B, sumLists(A, B))
+
