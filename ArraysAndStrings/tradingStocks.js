bfe65130da23b35adac52605134d4bffb91dd6e4
Wallace <wallace.s.luk@gmail.com>
2017-01-12 16:22:18 -0800

    Sloppy solution

--- ArraysAndStrings/tradingStocks.js
+++ ArraysAndStrings/tradingStocks.js
@@ -1,3 +1,5 @@
+// The front-page problem for interview cake
+
 // Suppose we had access to yesterday's stock prices as a list where:
 // 1) the indices are the time in minutes past the trade opening time which was 9:30 AM local time
 // 2) the values are the price in dollars of Apple stock at that time
@@ -13,6 +15,48 @@
 // No 'shortening' -- you must buy before you sell. You may not buy and sell in the same
 // time step (at least 1 minute must pass).
 
-const getMaxProfit = () => {
-  
+// [Brute force solution] -- O(n^2) where for each element you check every other
+// element ahead of it for the highest sale value. Every possible pair is checked until
+// you have the highest.
+// const getMaxProfit = (prices) => {
+//   let largest = 0;
+//   for(var i = 0; i < prices.length; i++) {
+//     for(var j = i + 1; j < prices.length; j++) {
+//       largest = Math.max(largest, prices[j] - prices[i]);
+//     }
+//   }
+//   return largest;
+// };
+
+// A more efficient solution
+// Traverse the list alternating between finding the lowest, and finding the highest
+const getMaxProfit = (prices) => {
+  let largest = 0;
+  let flag = true;
+  let low = prices[0];
+  let high = prices[0];
+
+  for(var i = 1; i < prices.length; i++) {
+    if(flag){
+      if(prices[i] <= prices[i-1]) {
+        low = Math.min(low, prices[i]);
+      } else { 
+        flag = !flag;
+        high = prices[i];
+      }
+    } else {
+      if(prices[i] <= prices[i-1]) {
+        flag = !flag;
+        largest = Math.max(largest, high - low);
+        low = prices[i]
+      } else {
+        high = Math.max(high, prices[i]);
+      }
+    }
+  }
+  return largest;
 };
+
+// Tests:
+const prices = [10, 7, 5 , 8 , 11, 9];
+console.log(getMaxProfit(prices));
