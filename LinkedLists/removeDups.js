346bb7d8765c8e75eb43ec506f9b51287aca2159
Wallace <wallace.s.luk@gmail.com>
2017-01-12 18:47:53 -0800

    add removeDups

--- LinkedLists/removeDups.js
+++ LinkedLists/removeDups.js
@@ -1,6 +1,35 @@
 const List = require('./List.js');
 
-const removeDup = () => {
+// Write code to remove dups from an unsroted linked list
+const removeDup = (list) => {
+  let iter = list.head;
+  let prev = null;
+  const mem = {};
+  
+  while(iter !== null) {
+    if(mem[iter.val] !== undefined) {
+      const next = iter.next;
+      prev.next = next;
+      iter = next;
+      continue;
+    }
 
+    mem[iter.val] = true;
+    prev = iter;
+    iter = iter.next;
+  }
 };
+
+// Tests
+const l = new List();
+l.push('a');
+l.push('b');
+l.push('a');
+l.push('c');
+l.push('d');
+l.push('a');
+console.log(''+l);
+removeDup(l);
+console.log(''+l);
+
 
