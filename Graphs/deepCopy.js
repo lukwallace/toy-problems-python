f5e193dc27a13fd578a76f70b99d3e1226439cb5
Wallace <wallace.s.luk@gmail.com>
2017-01-19 23:35:51 -0800

    setup

---
+++ Graphs/deepCopy.js
@@ -0,0 +1,9 @@
+// Given the root of a directed graph of the form { val: Value, edges: [Object] }
+// Write a function to create a deep copy of the graph. Assume all values
+// are unique.
+
+// Can you do it with a graph with duplicates?
+
+const deepCopy = (root) => {
+
+};
---
+++ idealVacation.js
@@ -0,0 +1,7 @@
+// Consider an array of integers where each index represents a day 
+// and each value a number corresponding to the tourist attraction.
+// Come up with a function that returns the length of minimum continous
+// sequence of days it will take to go to every attraction.
+
+// Example:
+// idealVacation([4, 5, 2, 1, 1, 4, 5, 3])  -> 6
