0f0ff37941ee3dc395b9951fc6ebd05e428ff8cd
Wallace <wallace.s.luk@gmail.com>
2017-01-27 13:02:20 -0800

    Setup

---
+++ TreesAndGraphs/routeBetweenNodes.js
@@ -0,0 +1,10 @@
+// Given a directed graph, design an algorithm to find out whether there is a route between two nodes
+// Assume a node is an object with and edges array
+
+const routeBetweenNodes = (A, B) => {
+  const queue = [A];
+  while(queue.length > 0) {
+    
+  }
+};
+
