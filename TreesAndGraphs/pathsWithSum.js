8307749ab1e1b1c7786aca0582fb34b6f46c52fe
Wallace <wallace.s.luk@gmail.com>
2017-02-13 16:27:10 -0800

    Polish pathsWithSum

--- TreesAndGraphs/pathsWithSum.js
+++ TreesAndGraphs/pathsWithSum.js
@@ -21,6 +21,7 @@
 let pathsWithSum = (tree, target) => {
   let sum = 0;
   let res = 0;
+  let cache = [];
   const OutterTraversal = (node) => {
     // base case -- bottom out go back up
     if(node === null) {
@@ -36,16 +37,54 @@ let pathsWithSum = (tree, target) => {
       return;
     }
     sum += node.val;
+    cache.push(node.val);
     if(sum === target) {
+      console.log(cache);
       res++;
     }
     innerTraversal(node.left);
     innerTraversal(node.right);
     sum -= node.val;
+    cache.pop(node.val);
   };
 
   OutterTraversal(tree);
   return res;
 };
 
-// Note to self, gotta build a tree building framework to make testing easy. 
+// Helper function, got too lazy to build trees manually, also better for extensive testing
+const makeTree = (arr) => {
+  if(arr.length === 0) {
+    return null;
+  }
+
+  let rootNode = { val: arr[0], left: null, right: null };
+  let currIndex = 0;
+  let temp = undefined;
+
+  const workQueue = [rootNode];
+  while(workQueue.length > 0) {
+    temp = workQueue.shift();
+    currIndex++;
+    if(arr[currIndex] !== undefined) {
+      const left = { val: arr[currIndex], left: null, right: null };
+      temp.left = left;
+      workQueue.push(left);
+    }
+
+    currIndex++;
+    if(arr[currIndex] !== undefined) {
+      const right = { val: arr[currIndex], left: null, right: null };
+      temp.right = right;
+      workQueue.push(right);
+    }
+  }
+  return rootNode;
+};
+
+const tree = makeTree([10,5,4,3,1,2,7,5,0,2,3,4,5,6,0]);
+console.log(pathsWithSum(tree, 8));
+
+// A better implementation would could have an object store the value of the running sum. I.e. at each node, make an entry in the object indicating that can reach this sum. Uses only a single traversal from the root node. To get the paths that sum up to the target that does not start from the root, we can subtract the target value from the current running sum and see if we can reach that partial sum by looking it up in our object. Example: Object { '10': 1, '15': 1, '18': 1 } with RunningSum: 18. Calculate 18 - 8 = 10. Can we reach 10? Yes - Object['10'] is 1 so we can get to 10 by one path.
+
+
