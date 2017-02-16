f0493ea9dea52f457af866fc0ed1c5bdbca54bbe
Wallace <wallace.s.luk@gmail.com>
2017-02-15 17:26:58 -0800

    Add not-particularly interesting

---
+++ BitManipulation/debugger.js
@@ -0,0 +1,7 @@
+// Explain what the following code does: (( n & (n - 1)) === 0)
+
+// My answer:
+// It checks if n is a number that only contains one 1. 
+
+// Actual anser:
+// checks if then number is a power of two (containing only one 1 visual result);
---
+++ BitManipulation/pairwiseSwap.js
@@ -0,0 +1,5 @@
+// Fewest instructions to do a pairwise swap
+// 1010011101010001
+// 0101101110100010
+
+console.log((1 << 31).toString(2));
